99b063aa606994f0812693209ee9a9d3100421fa
prabhuomkar <prabhuomkar@pm.me>
2020-07-29 18:48:54 +0530

    pylint

--- torchexpo/vision/image_classification/googlenet.py
+++ torchexpo/vision/image_classification/googlenet.py
@@ -11,7 +11,7 @@ class GoogleNet(TorchExpo):
     example = torch.rand(1, 3, 224, 224)
 
     def __init__(self):
-        """Initialize Model"""        
+        """Initialize Model"""
         self.model = torchvision.models.googlenet(pretrained=True)
         super().__init__(self.model, self.name, self.example)
 
