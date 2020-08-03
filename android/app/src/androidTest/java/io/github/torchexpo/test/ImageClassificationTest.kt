8428a418c48d4f896008bf70405f655f4c2c3bca
prabhuomkar <prabhuomkar@pm.me>
2020-08-03 18:45:15 +0530

    Updates to Android

--- android/app/src/androidTest/java/io/github/torchexpo/test/ImageClassificationTest.kt
+++ android/app/src/androidTest/java/io/github/torchexpo/test/ImageClassificationTest.kt
@@ -3,14 +3,15 @@ package io.github.torchexpo.test
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ImageClassificationTest {
 
     private lateinit var activityScenario: ActivityScenario<MainActivity>
     private val imagenetResult = "white wolf, Arctic wolf, Canis lupus tundrarum"
-    private var intent: Intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
+    private var intent: Intent =
+        Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
 
     @Test
     fun testResNet18() {
--- android/app/src/main/java/io/github/torchexpo/test/MainActivity.kt
+++ android/app/src/main/java/io/github/torchexpo/test/MainActivity.kt
@@ -1,20 +1,22 @@
 package io.github.torchexpo.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import io.github.torchexpo.test.vision.ImageClassification
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var modelName: String
+    private var modelName: String? = null
     private lateinit var result: String;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        modelName = intent.getStringExtra("modelName").toString()
-        result = ImageClassification(this, modelName).predict()
+        modelName = intent.getStringExtra("modelName")
+        if (!modelName.isNullOrEmpty()) {
+            result = ImageClassification(this, modelName!!).predict()
+        }
     }
 
     fun getResult(): String = result
