a90db09c683cec902dec464fd899776686c018f3
Omkar Prabhu <prabhuomkar@pm.me>
2022-11-05 21:42:37 +0530

    some more cleanup

--- scripts/example.py
+++
@@ -1,8 +0,0 @@
-import torchexpo
-from torchexpo.vision import image_classification
-
-
-print(torchexpo.__version__)
-model = image_classification.resnet18()
-model.extract_torchscript()
-model.extract_onnx()
