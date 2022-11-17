476762994d7ff2193f31b6a55f24575afcba9409
Omkar Prabhu <prabhuomkar@pm.me>
2022-11-17 16:33:49 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,26 @@ from setuptools import setup, find_packages
 
 
 version = "2.0.0-alpha1"
-sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8") 
 package_name = "torchexpo"
 
 cwd = os.path.dirname(os.path.abspath(__file__))
+
 
 def write_version_file():
     version_path = os.path.join(cwd, package_name, "version.py")
     with open(version_path, "w") as f:
         f.write("__version__ = '{}'\n".format(version))
-        f.write("git_version = {}\n".format(repr(sha)))
+
 
 write_version_file()
 
-readme = open("README.md").read()
+with open(os.path.join(cwd, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
-requirements = open("requirements.txt").read().split()
+install_requires = [
+    "torch",
+    "torchvision"
+]
 
 setup(
     name=package_name,
@@ -27,18 +31,26 @@ setup(
     author="Omkar Prabhu",
     author_email="prabhuomkar@pm.me",
     url="https://github.com/torchexpo/torchexpo",
+    download_url="https://github.com/torchexpo/torchexpo/tags",
     description="Collection of models and extensions for deployment in PyTorch",
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
     packages=find_packages(exclude=("tests", "docs", "examples", "scripts")),
     zip_safe=True,
-    install_requires=requirements,
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
     ],
 )
